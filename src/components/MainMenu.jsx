e0e6e2e7b9fc1eda2e90b4e6962a0e4b23b41fa8
Enrique <bosspetta@users.noreply.github.com>
2023-07-18 12:40:13 +0200

    Quitamos el js externo a la aplicación

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,41 +1,53 @@
 import { NavLink } from "react-router-dom"
 
 export default function MainMenu() {
+
+    const hideMenu = () => {
+        document.body.classList.remove('menu-opened')
+        document.getElementById('main-menu').classList.remove('menu-opened')
+        document.getElementById('btn-menu').classList.remove('is-active')
+    }
+
     return (
         <nav id="main-menu" className="main-menu">
             <ul className="main-menu__items">
                 <li className="main-menu__item">
                     <NavLink
                         className={ ({isActive}) => isActive ? 'main-menu__link selected' : 'main-menu__link' }
-                        to='/'>
+                        to='/'
+                        onClick={hideMenu}>
                         Inicio
                     </NavLink>
                 </li>
                 <li className="main-menu__item">
                     <NavLink
                         className={ ({isActive}) => isActive ? 'main-menu__link selected' : 'main-menu__link' }
-                        to='/que-es-enon'>
+                        to='/que-es-enon'
+                        onClick={hideMenu}>
                         ¿Qué es <strong>enON</strong>?
                     </NavLink>
                 </li>
                 <li className="main-menu__item">
                     <NavLink
                         className={ ({isActive}) => isActive ? 'main-menu__link selected' : 'main-menu__link' }
-                        to='/yoga'>
+                        to='/yoga'
+                        onClick={hideMenu}>
                         Yoga
                     </NavLink>
                 </li>
                 <li className="main-menu__item">
                     <NavLink
                         className={ ({isActive}) => isActive ? 'main-menu__link selected' : 'main-menu__link' }
-                        to='/quiromasaje'>
+                        to='/quiromasaje'
+                        onClick={hideMenu}>
                         Quiromasaje
                     </NavLink>
                 </li>
                 <li className="main-menu__item">
                     <NavLink
                         className={ ({isActive}) => isActive ? 'main-menu__link selected' : 'main-menu__link' }
-                        to='/contacto'>
+                        to='/contacto'
+                        onClick={hideMenu}>
                         Visítanos / Contacto
                     </NavLink>
                 </li>
