8d968e07e204d3f3878c40f9d6803115c3d61bbe
erodriguezv <erodriguezv@minsait.com>
2023-07-25 10:35:26 +0200

    Yoga texts

--- src/pages/Yoga.jsx
+++ src/pages/Yoga.jsx
@@ -12,6 +12,10 @@ export default function Yoga() {
     return (
         <main className="page-content">
             <h2 className="page-title">{t("yoga.title")}</h2>
+            <h3 className="page-title--subtitle">{t("yoga.enon.title")}</h3>
+            <p>{t( "yoga.enon.p1" )}</p>
+            <p>{t( "yoga.enon.p2" )}</p>
+            <p>{t( "yoga.enon.p3" )}</p>
             <h3 className="page-title--subtitle">{t( "yoga.subtitle" )}</h3>
             <p>{t( "yoga.hatha.paragraph-1" )}</p>
             <p>{t( "yoga.hatha.paragraph-2" )}</p>
