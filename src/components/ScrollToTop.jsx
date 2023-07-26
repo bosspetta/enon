84b3b201c778e1fe7a0b3a3b3f5f8c479d97eb61
erodriguezv <erodriguezv@minsait.com>
2023-07-26 11:30:35 +0200

    prop-types added

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import PropTypes from 'prop-types'
 import { useLocation } from 'react-router'
 
 const ScrollToTop = (props) => {
@@ -8,6 +9,10 @@ const ScrollToTop = (props) => {
     }, [location])
 
     return <>{props.children}</>
+}
+
+ScrollToTop.propTypes = {
+    children: PropTypes.object
 }
 
 export default ScrollToTop
