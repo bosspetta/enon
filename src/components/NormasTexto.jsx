851ba6c79de8cbfb7a78df82ef332b3fe255ee74
erodriguezv <erodriguezv@minsait.com>
2023-09-06 11:56:11 +0200

    Rules translations

--- src/components/NormasTexto.jsx
+++ src/components/NormasTexto.jsx
@@ -1,134 +1,138 @@
+import { useTranslation } from 'react-i18next'
+// {t('rules.')}
 export default function NormasTexto() {
+
+    const { t } = useTranslation('global')
+
     return (
         <>
-            <h3 className="page-title--subtitle">Políticas y Reglamento alumnos inscritos y eventuales</h3>
+            <h3 className="page-title--subtitle">{t('rules.main-title')}</h3>
 
-            <p>Serán considerados alumnos inscritos aquellos que abonen la mensualidad regularmente. Serán considerados alumnos eventuales, todos aquellos quienes tomen clases sueltas. Todos los alumnos (inscritos y eventuales) deberán leer el siguiente reglamento de inicio y políticas de enON Estudio de Yoga. Adicionalmente, deberán firmar el consentimiento que se les entregará al momento de efectuar la inscripción. Las personas que no cumplan con las siguientes normas y obligaciones, no tendrán derecho a quejas o reclamaciones si se les negara o se les prohibiera el uso del estudio de yoga.</p>
+            <p>{t('rules.p-1')}</p>
 
-            <h3 className="page-title--subtitle">Reglamento de inicio</h3>
+            <h3 className="page-title--subtitle">{t('rules.title-2')}</h3>
 
-            <p><strong>PRESENTACIÓN PERSONAL</strong></p>
-
-            <ul>
-                <li>Se debe respetar el código de vestimenta para la participación en las clases (ropa deportiva).</li>
-                <li>En la sala está prohibido el uso de zapatos.</li>
-                <li>El uso de zapatos está permitido únicamente en el área de recepción y deben quedar en el vestuario.</li>
-                <li>Asegúrese de apagar su teléfono antes de ingresar a las clases.</li>
-            </ul>
-
-            <p><strong>PREPARACIÓN PARA LA CLASE, PUNTUALIDAD</strong></p>
+            <p><strong>{t('rules.title-3')}</strong></p>
 
             <ul>
-                <li>Es importante llegar 10 minutos antes de la hora establecida para el inicio de la clase, de esa forma la clase comienza puntualmente a su hora.</li>
-                <li>Una vez se empieza la clase, hay 5 minutos de tolerancia para el inicio. Después, la puerta se cierra restringiendo el ingreso al salón.</li>
-                <li>No se permite la llegada tarde a clase, con el objetivo de promover la disciplina y respeto del tiempo, tanto de los asistentes como de los profesores.</li>
-                <li>La puerta se cerrará una vez iniciada la clase.</li>
+                <li>{t('rules.list-1.li-1')}</li>
+                <li>{t('rules.list-1.li-2')}</li>
+                <li>{t('rules.list-1.li-3')}</li>
+                <li>{t('rules.list-1.li-4')}</li>
             </ul>
 
-            <p><strong>HIDRATACIÓN</strong></p>
+            <p><strong>{t('rules.title-4')}</strong></p>
 
             <ul>
-                <li>No ingerir ningún alimento 2 horas antes de cada clase.</li>
-                <li>El consumo de agua durante la práctica puede interferir en la realización de los ejercicios, u ocasionar molestias, es más aconsejable beber al finalizar la práctica.</li>
+                <li>{t('rules.list-2.li-1')}</li>
+                <li>{t('rules.list-2.li-2')}</li>
+                <li>{t('rules.list-2.li-3')}</li>
+                <li>{t('rules.list-2.li-4')}</li>
             </ul>
 
-            <p><strong>INGRESO</strong></p>
+            <p><strong>{t('rules.title-5')}</strong></p>
 
             <ul>
-                <li>Ingrese al salón haciendo el menor ruido posible para respetar a los compañeros que se encuentran meditando o en proceso de relajación.</li>
-                <li>Acomode su esterilla y siéntese o acuéstese en silencio hasta que inicie la práctica.</li>
-                <li>Si llega temprano, utilice el tiempo para relajarse o meditar en su esterilla. Esto prepara el cuerpo y la mente para la práctica. Recuerde que con la práctica buscamos la conexión con nosotros mismos y esto abre la posibilidad de que se manifiesten emociones durante la misma.</li>
-                <li>No es permitido el ingreso de móviles, reloj u otro dispositivo electrónico.</li>
-                <li>Recuerde no utilizar anillos, cadenas, relojes ni demás elementos que puedan entorpecer la práctica y dañar los mismos.</li>
+                <li>{t('rules.list-3.li-1')}</li>
+                <li>{t('rules.list-3.li-2')}</li>
             </ul>
 
-            <p><strong>DURANTE LA PRÁCTICA</strong></p>
+            <p><strong>{t('rules.title-6')}</strong></p>
 
             <ul>
-                <li>Debe ser respetada la rutina del profesor el cual está capacitado para desarrollar metodológicamente la clase.</li>
-                <li>El profesor dará indicaciones, hará correcciones de las posturas de yoga, de modo que la clase sea segura y armónica para todos los alumnos.</li>
-                <li>Durante las clases, se debe mantener el silencio.</li>
-                <li>Dudas y preguntas se responderán antes y después de la clase.</li>
-                <li>Tómese un descanso cuando lo crea necesario.</li>
-                <li>Importante, durante la clase no excederse e ir por delante del dolor. Menos es más.</li>
-                <li>Es imperativo que se avise al instructor en caso de presentarse una molestia o dolor durante la práctica.</li>
+                <li>{t('rules.list-4.li-1')}</li>
+                <li>{t('rules.list-4.li-2')}</li>
+                <li>{t('rules.list-4.li-3')}</li>
+                <li>{t('rules.list-4.li-4')}</li>
+                <li>{t('rules.list-4.li-5')}</li>
             </ul>
 
-            <p><strong>USO DE LA INFRAESTRUCTURA</strong></p>
+            <p><strong>{t('rules.title-7')}</strong></p>
 
             <ul>
-                <li>Todo elemento utilizado durante la práctica debe ser guardado de manera correspondiente nuevamente en su lugar.</li>
-                <li>Las horas de atención al público, son media hora antes del inicio de cada clase.</li>
-                <li>Si necesita conversar con algún compañero, sea preciso y hágalo en voz baja, de preferencia en el área de vestuarios.</li>
-                <li>El salón es un espacio que permite adentrarse y soltar. Debe ser seguro, por lo tanto, no caben comentarios respecto a lo que sucede en él.</li>
+                <li>{t('rules.list-5.li-1')}</li>
+                <li>{t('rules.list-5.li-2')}</li>
+                <li>{t('rules.list-5.li-3')}</li>
+                <li>{t('rules.list-5.li-4')}</li>
+                <li>{t('rules.list-5.li-5')}</li>
+                <li>{t('rules.list-5.li-6')}</li>
+                <li>{t('rules.list-5.li-7')}</li>
             </ul>
 
-            <p><strong>USO DEL VESTUARIO</strong></p>
+            <p><strong>{t('rules.title-8')}</strong></p>
 
             <ul>
-                <li>Cada persona puede traer una toalla de uso personal.</li>
-                <li>Pero debe llevársela, no se permite guardar nada ningún objeto personal en el estudio.</li>
-                <li>Asegúrese de apagar su teléfono antes de ingresar a la clase.</li>
+                <li>{t('rules.list-6.li-1')}</li>
+                <li>{t('rules.list-6.li-2')}</li>
+                <li>{t('rules.list-6.li-3')}</li>
+                <li>{t('rules.list-6.li-4')}</li>
             </ul>
 
-            <p><strong>POLÍTICAS enON ESTUDIO DE YOGA</strong></p>
-
-            <p>Las personas que practican dentro de las instalaciones de enON Estudio de Yoga están de acuerdo con las siguientes condiciones.</p>
-
-            <p><strong>PAGOS</strong></p>
-
-            <p>Las clases se abonarán durante la primera semana del mes (siete primeros días del mes). Todos los meses se abonará el mes completo, independientemente de si se viene a clase o no (por el motivo que sea), o del número de clases que tenga el mes. El Pago de la mensualidad puede hacerse en efectivo. Al realizar el pago en efectivo, el alumno/a habrá de firmar en la Hoja de Control de Pagos como método de verificación del abono de la mensualidad correspondiente.</p>
+            <p><strong>{t('rules.title-9')}</strong></p>
 
             <ul>
-                <li>Cada mes cuenta con 8 clases de yoga, 2 por semana.</li>
-                <li>Para medio mes cuenta con 4 clases 1 por semana.</li>
-                <li>La persona debe elegir día fijo para su práctica donde le será reservada su plaza.</li>
-                <li>En caso de que el centro de yoga suspenda alguna clase, los alumnos presentes podrán recuperarla.</li>
-                <li>Si el alumno no puede asistir a su clase, esta no se recupera.</li>
+                <li>{t('rules.list-7.li-1')}</li>
+                <li>{t('rules.list-7.li-2')}</li>
+                <li>{t('rules.list-7.li-3')}</li>
             </ul>
 
-            <p><strong>CLASES SUELTAS</strong></p>
+            <p><strong>{t('rules.title-10')}</strong></p>
+
+            <p>{t('rules.p-2')}</p>
+
+            <p><strong>{t('rules.title-11')}</strong></p>
 
             <ul>
-                <li>Los alumnos que no deseen pagar una mensualidad, podrán pagar clases sueltas al costo vigente al momento que se tome la clase.</li>
-                <li>El pago de la clase suelta deberá realizarse el mismo día en que se toma la clase.</li>
-                <li>Solo podrá asistir si queda plazas libres.</li>
+                <li>{t('rules.list-8.li-1')}</li>
+                <li>{t('rules.list-8.li-2')}</li>
+                <li>{t('rules.list-8.li-3')}</li>
+                <li>{t('rules.list-8.li-4')}</li>
+                <li>{t('rules.list-8.li-5')}</li>
+                <li>{t('rules.list-8.li-6')}</li>
             </ul>
 
-            <p><strong>REEMBOLSOS</strong></p>
+            <p><strong>{t('rules.title-12')}</strong></p>
 
             <ul>
-                <li>No se hacen reembolsos de dinero una vez se haya efectuado el pago.</li>
-                <li>Se recomienda a los alumnos ser parte de la lista de correos de enON Estudio de Yoga. Para poder recibir los boletines, el alumno deberá agregar como contacto el correo hola@enon.yoga</li>
-                <li>Los avisos importantes y boletines del centro de yoga, serán enviados a los alumnos a través de correo electrónico, o por vía WhatsApp, por lo que será responsabilidad de cada alumno estar pendiente de algún aviso importante.</li>
+                <li>{t('rules.list-9.li-1')}</li>
+                <li>{t('rules.list-9.li-2')}</li>
+                <li>{t('rules.list-9.li-3')}</li>
             </ul>
 
-            <p>Es responsabilidad de los alumnos mantener la información actualizada (cambio de número de teléfono, correo electrónico, etc.) en su formato de inscripción.</p>
-
-            <p><strong>BAJA DE CLASES DE YOGA</strong></p>
-
-            <p>El alumno/a deberá comunicar claramente por palabra, vía WhatsApp o correo electrónico, su intención de darse de baja de las clases de Yoga. La baja deberá comunicarse antes del día 1 de cada mes. Si la baja se produce después del día 1, a mitad o finales de mes, por el motivo que sea, el alumno/a se compromete a pagar la mensualidad completa de ese mes. Aunque el alumno/a no haya asistido a ninguna clase ese mes, puesto que ha ocupado una plaza en la escuela, deberá abonar igualmente esa mensualidad.</p>
-
-            <p><strong>Política de privacidad</strong></p>
-
-            <p>Todos los datos personales proporcionados serán tratados únicamente con fines comerciales y se almacenarán siguiendo la normativa de protección de datos. Tratamos toda la información personal como confidencial. En ninguna circunstancia proporcionaremos información personal a terceros no autorizados y en todo momento protegeremos sus datos contra cualquier acceso no autorizado. El alumno debe aceptar recibir el boletín de noticias y ofertas especiales de enON Estudio de Yoga.</p>
-
-            <p><strong>Descargo de responsabilidad y posibles riesgos</strong></p>
-
-            <p>enON ESTUDIO DE YOGA no se hará responsable de aquellos alumnos que no sigan las directrices del instructor. Cualquier condición médica o física, lesiones recientes o presientes, así como cualquier tipo de información relevante para la práctica del alumno, debe ser comunicado al instructor antes de comenzar la clase. Es responsabilidad del alumno comunicarle al instructor se siente cualquier dolor o molestia física durante la clase. El uso de las instalaciones implica que el alumno acepta el riesgo asociado con todas las formas de ejercicio físico, tanto antes como después de la clase. Si tiene dudas acerca de su capacidad para poder participar en nuestras clases debido a su condición médica, por favor consultarlo con su médico. enON ESTUDIO DE YOGA y los miembros de su equipo, no se harán responsable en los siguientes casos:</p>
+            <p><strong>{t('rules.title-13')}</strong></p>
 
             <ul>
-                <li>Cualquier lesión o daño que resulte de la participación o uso de nuestras instalaciones.</li>
-                <li>Cualquier daño o pérdida de objetos personales.</li>
+                <li>{t('rules.list-10.li-1')}</li>
+                <li>{t('rules.list-10.li-2')}</li>
+                <li>{t('rules.list-10.li-3')}</li>
             </ul>
 
-            <p><strong>Objetos personales</strong></p>
+            <p>{t('rules.p-3')}</p>
 
-            <p>enON ESTUDIO DE YOGA no se hará responsable de aquellos objetos personales olvidados o que queden en nuestro local.</p>
+            <p><strong>{t('rules.title-14')}</strong></p>
 
-            <p><strong>Derechos afines</strong></p>
+            <p>{t('rules.p-4')}</p>
 
-            <p>enON ESTUDIO DE YOGA se reserva el derecho de modificar los términos y condiciones en cualquier momento. Los cambios importantes serán comunicados a través de nuestra página web. Si tienes dudas o preguntas acerca de nuestros términos y condiciones, por favor contáctanos vía email a hola@enon.yoga</p>
+            <p><strong>{t('rules.title-15')}</strong></p>
+
+            <p>{t('rules.p-5')}</p>
+
+            <p><strong>{t('rules.title-16')}</strong></p>
+
+            <p>{t('rules.p-6')}</p>
+
+            <ul>
+                <li>{t('rules.list-11.li-1')}</li>
+                <li>{t('rules.list-11.li-2')}</li>
+            </ul>
+
+            <p><strong>{t('rules.title-17')}</strong></p>
+
+            <p>{t('rules.p-7')}</p>
+
+            <p><strong>{t('rules.title-18')}</strong></p>
+
+            <p>{t('rules.p-8')}</p>
         </>
     )
 }
