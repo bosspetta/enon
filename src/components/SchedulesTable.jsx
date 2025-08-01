c8b3d7e5c98b670256751a7f71e7514d08901cae
bosspetta <bosspetta@proton.me>
2025-08-01 11:55:29 +0200

    Renovación de precios.

--- src/components/SchedulesTable.jsx
+++ src/components/SchedulesTable.jsx
@@ -29,23 +29,39 @@ export default function SchedulesTable() {
                     <td headers="schedules-tableColHdr3 schedules-tableRowHdr2"></td>
                     <td headers="schedules-tableColHdr4 schedules-tableRowHdr2">Hatha Yoga</td>
                     <td headers="schedules-tableColHdr5 schedules-tableRowHdr2"></td>
-                    <td headers="schedules-tableColHdr6 schedules-tableRowHdr2"></td>
+                    <td headers="schedules-tableColHdr6 schedules-tableRowHdr2">Yoga Restaurativo</td>
                 </tr>
                 <tr>
-                    <th id="schedules-tableRowHdr4">19:00</th>
+                    <th id="schedules-tableRowHdr3">18:30</th>
                     <td headers="schedules-tableColHdr2 schedules-tableRowHdr3"></td>
                     <td headers="schedules-tableColHdr3 schedules-tableRowHdr3"></td>
                     <td headers="schedules-tableColHdr4 schedules-tableRowHdr3"></td>
-                    <td headers="schedules-tableColHdr5 schedules-tableRowHdr3"></td>
+                    <td headers="schedules-tableColHdr5 schedules-tableRowHdr3">Mindfulnes</td>
                     <td headers="schedules-tableColHdr6 schedules-tableRowHdr3"></td>
                 </tr>
                 <tr>
-                    <th id="schedules-tableRowHdr4">19:30</th>
-                    <td headers="schedules-tableColHdr2 schedules-tableRowHdr4">Hatha Yoga</td>
-                    <td headers="schedules-tableColHdr3 schedules-tableRowHdr4">Hatha Yoga</td>
-                    <td headers="schedules-tableColHdr4 schedules-tableRowHdr4">Hatha Yoga</td>
+                    <th id="schedules-tableRowHdr4">19:00</th>
+                    <td headers="schedules-tableColHdr2 schedules-tableRowHdr4"></td>
+                    <td headers="schedules-tableColHdr3 schedules-tableRowHdr4"></td>
+                    <td headers="schedules-tableColHdr4 schedules-tableRowHdr4"></td>
                     <td headers="schedules-tableColHdr5 schedules-tableRowHdr4"></td>
                     <td headers="schedules-tableColHdr6 schedules-tableRowHdr4"></td>
+                </tr>
+                <tr>
+                    <th id="schedules-tableRowHdr4">19:30</th>
+                    <td headers="schedules-tableColHdr2 schedules-tableRowHdr5">Hatha Yoga</td>
+                    <td headers="schedules-tableColHdr3 schedules-tableRowHdr5"></td>
+                    <td headers="schedules-tableColHdr4 schedules-tableRowHdr5">Hatha Yoga</td>
+                    <td headers="schedules-tableColHdr5 schedules-tableRowHdr5"></td>
+                    <td headers="schedules-tableColHdr6 schedules-tableRowHdr5"></td>
+                </tr>
+                <tr>
+                    <th id="schedules-tableRowHdr4">20:00</th>
+                    <td headers="schedules-tableColHdr2 schedules-tableRowHdr6"></td>
+                    <td headers="schedules-tableColHdr3 schedules-tableRowHdr6">Hatha Yoga</td>
+                    <td headers="schedules-tableColHdr4 schedules-tableRowHdr6"></td>
+                    <td headers="schedules-tableColHdr5 schedules-tableRowHdr6">Hatha Yoga</td>
+                    <td headers="schedules-tableColHdr6 schedules-tableRowHdr6"></td>
                 </tr>
             </tbody>
         </table>
--- src/pages/SchedulesPrices.jsx
+++ src/pages/SchedulesPrices.jsx
@@ -21,36 +21,43 @@ export default function SchedulesPrices() {
             <h2 className="page-title">{t( "schedules.title" )}</h2>
             <h3 className="page-title--subtitle align-center">{t("schedules.schedules")}</h3>
             <SchedulesTable />
-            <p className="align-center">* <strong><small>{t( "schedules.duration" )}</small></strong></p>
             <div className="schedules-mobile">
                 <p><strong>{t('schedules.monday')}: </strong><br />
                     18:00 - Hatha Yoga<br />
                     19:30 - Hatha Yoga</p>
                 <p><strong>{t('schedules.tuesday')}: </strong><br />
                     10:00 - Hatha Yoga<br />
-                    19:30 - Hatha Yoga</p>
+                    20:00 - Hatha Yoga</p>
                 <p><strong>{t('schedules.wednesday')}: </strong><br />
                     18:00 - Hatha Yoga<br />
                     19:30 - Hatha Yoga</p>
                 <p><strong>{t('schedules.thursday')}: </strong><br />
-                    10:00 - Hatha Yoga</p>
+                    10:00 - Hatha Yoga<br />
+                    18:30 - Mindfulnes<br />
+                    20:00 - Hatha Yoga</p>
+                <p><strong>{t('schedules.friday')}: </strong><br />
+                    18:00 - Taller Yoga Restaurativo</p>
             </div>
             <h3 className="page-title--subtitle align-center">{t("schedules.subtitle")}</h3>
             <ul className="prices">
                 <li><span className="prices__label">{t("schedules.1class")}</span> <span className="prices__price">{t("schedules.1class-price")}</span></li>
                 <li><span className="prices__label">{t("schedules.2class")}</span> <span className="prices__price">{t("schedules.2class-price")}</span></li>
                 <li><span className="prices__label">{t("schedules.3class")}</span> <span className="prices__price">{t("schedules.3class-price")}</span></li>
+
+                <li><span className="prices__label">{t("schedules.taller-restaurativo")}</span> <span className="prices__price">{t("schedules.taller-restaurativo-price")}</span></li>
+                <li><span className="prices__label">{t("schedules.mindfulnes")}</span> <span className="prices__price">{t("schedules.mindfulnes-price")}</span></li>
+
                 <li><span className="prices__label">{t('schedules.clase-suelta')}</span> <span className="prices__price">15€</span></li>
-                <li><span className="prices__label">{t('schedules.clase-prueba')}</span> <span className="prices__price">15€</span></li>
             </ul>
             <hr />
             <p>{t('schedules.normas')} <Link to="/normas">{t('schedules.normas-link')}</Link>.</p>
             <hr />
             <h3 className="page-title--subtitle">{t("schedules.masajes")}</h3>
             <ul>
-                <li>{t( "schedules.p-quiro" )}, <strong>38€</strong></li>
-                <li>{t( "schedules.p-relax" )}, <strong>40€</strong></li>
-                <li>{t( "schedules.p-facial" )}, <strong>40€</strong></li>
+                <li>{t( "schedules.p-quiro" )}, <strong>40€</strong></li>
+                <li>{t( "schedules.p-relax" )}, <strong>43€</strong></li>
+                <li>{t( "schedules.p-relax-90" )}, <strong>65€</strong></li>
+                <li>{t( "schedules.p-facial" )}, <strong>45€</strong></li>
             </ul>
         </main>
     )
