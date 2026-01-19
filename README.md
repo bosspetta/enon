f9de723b04e6bd21198d012051da54e02420a6a1
bosspetta <bosspetta@proton.me>
2026-01-19 09:06:24 +0100

    Gift card.

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Corporate website for enON - Yoga and Chiromassage studio
 ## Dev mode
 
 npm run dev
+sass --watch src/scss:public/css
--- src/components/ChristmasPopup.jsx
+++ src/components/ChristmasPopup.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 
+import icoGift from '../assest/img/ico-gift.svg'
+
 export default function ChristmasPopup() {
 
     const { t } = useTranslation('global')
@@ -17,21 +19,30 @@ export default function ChristmasPopup() {
     }
 
     return (
-        <div className={`gif-carg-popup gif-carg-popup__${chPopup}`}>
-            <div className="gif-carg-popup__inner">
-                <header className="gif-carg-popup__header">
-                    <div className="gif-carg-popup__content">
-                        <h3 className="page-title--subtitle">{ t( "tarjeta-popup.title" ) }</h3>
-                        <button type="button" onClick={() => openChPopup()}><span className="sr-only">{ t( "tarjeta-popup.close" ) }</span></button>
+        <>
+
+            <div className={`gif-carg-popup gif-carg-popup__${chPopup}`}>
+                <div className="gif-carg-popup__inner">
+                    <header className="gif-carg-popup__header">
+                        <div className="gif-carg-popup__content">
+                            <h3 className="page-title--subtitle"><span className="sr-only">{ t( "tarjeta-popup.title" ) }</span></h3>
+                            <button type="button" onClick={() => openChPopup()} className="gif-carg-popup__btn">
+                                <span className="gif-carg-popup__btn__label">{ t( "tarjeta-popup.open" ) }</span>
+                                <img src={icoGift} alt="" className="gif-carg-popup__btn__ico" />
+                            </button>
+                        </div>
+                    </header>
+                    <div className="gif-carg-popup__content gif-carg-popup__data">
+                        <div className="gif-carg-popup__data__inner">
+                            <button type="button" onClick={() => closeChPopup()} className="gif-carg-popup__btn--close"><span className="sr-only">{t("tarjeta-popup.close")}</span></button>
+                            <p>{ t( "tarjeta-popup.p1" ) }</p>
+                            <p><Link to="/bono-regalo" className="gif-carg-popup__link" onClick={() => closeChPopup()}>{ t( "tarjeta-popup.p2" ) }</Link></p>
+                            <p>{ t( "tarjeta-popup.p3" ) }</p>
+                            <Link to="/bono-regalo" className="intro-links__link intro-links__link--alone" onClick={() => closeChPopup()}>{ t( "tarjeta-popup.btn" ) }</Link>
+                        </div>
                     </div>
-                </header>
-                <div className="gif-carg-popup__content">
-                    <p>{ t( "tarjeta-popup.p1" ) }</p>
-                    <p><Link to="/bono-regalo" className="gif-carg-popup__link" onClick={() => closeChPopup()}>{ t( "tarjeta-popup.p2" ) }</Link></p>
-                    <p>{ t( "tarjeta-popup.p3" ) }</p>
-                    <Link to="/bono-regalo" className="intro-links__link intro-links__link--alone" onClick={() => closeChPopup()}>{ t( "tarjeta-popup.btn" ) }</Link>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
